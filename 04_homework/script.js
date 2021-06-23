580691af025c01f841079c055b01c83c73d2bc69
metalspell <lonelyrockclimber@gmail.com>
2021-06-23 16:35:07 +0300

    New design of weather app

--- 04_homework/script.js
+++ 04_homework/script.js
@@ -7,7 +7,7 @@ function citySelect() {
 
 
 function getWeather() {
-  fetch("http://api.openweathermap.org/data/2.5/weather?id="+cityId+"&appid=b078a7d81849e7d2b796924e54583a40")
+  fetch("http://api.openweathermap.org/data/2.5/weather?id=" + cityId + "&appid=b078a7d81849e7d2b796924e54583a40")
     .then(function (resp) { return resp.json() })
     .then(function (data) {
       console.log(data);
@@ -17,12 +17,12 @@ function getWeather() {
       document.querySelector('.max').innerHTML = `Maximal temperature:      <span>${Math.round(data.main.temp_max - 273)}&deg;</span>`;
       document.querySelector('.min').innerHTML = `Minimal temperature:      <span>${Math.round(data.main.temp_min - 273)}&deg;</span>`;
       document.querySelector('.feel').innerHTML = `Feels like:      <span>${Math.round(data.main.feels_like - 273)}&deg;</span>`;
-      document.querySelector('.clouds').innerHTML = `Cloudiness:     <span>${data.weather[0]['description']}</span>`;
-      document.querySelector('.visibility').innerHTML = `Visibility:     <span>${data.visibility}</span>`;
-      document.querySelector('.wind-speed').innerHTML = `Wind speed:      <span> ${data.wind.speed}m/s</span>`;
-      // document.querySelector('.clouds-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
-      // document.querySelector('.preasure').innerHTML = `Preasure:     <span>${data.main.pressure}<span>`;
-      document.querySelector('.humidity').innerHTML = `Humidity:      ${data.main.humidity}&#37;`;
+      document.querySelector('.clouds').innerHTML = `<span>${data.weather[0]['description']}</span>`;
+      document.querySelector('.visibility').innerHTML = `Visibility:     <span>${data.visibility} m</span>`;
+      document.querySelector('.wind-speed').innerHTML = `Wind speed:      <span> ${data.wind.speed} m/s</span>`;
+      document.querySelector('.clouds-icon').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
+      document.querySelector('.preasure').innerHTML = `Preasure:<span>${data.main.pressure} hpa</span>`;
+      document.querySelector('.humidity').innerHTML = `Humidity:<span>${data.main.humidity}&#37</span>`;
     })
     .catch(function () {
       // catch any errors
